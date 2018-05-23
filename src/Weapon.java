0028c1b0ce2c8103284714aba5395e5d86cae44e
s012556 <s012556@lm-19s012556.lmsd.org>
2018-05-23 13:36:15 -0400

    hi

---
+++ src/Weapon.java
@@ -0,0 +1,40 @@
+
+public class Weapon {
+
+private int health;
+private int hitValue;
+
+public Weapon(){
+	this(30,10);
+}
+public Weapon(int health, int hitValue){
+	this.health=health;
+	this.hitValue=hitValue;
+}
+
+
+
+public int getHitValue() {
+	return hitValue;
+}
+
+public void setHitValue(int hitValue) {
+	this.hitValue = hitValue;
+}
+
+public int getHealth() {
+	return health;
+}
+
+public void setHealth(int health) {
+	this.health = health;
+}
+
+public void checkHealth(){
+	health--;
+	if(health<=0){
+		this.setHitValue(0);
+	}
+}
+
+}
